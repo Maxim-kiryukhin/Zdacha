1fe53fd589bc579e4e0e48b66d5cde98441c9b69
Maxim-kiryukhin <147792917+Maxim-kiryukhin@users.noreply.github.com>
2026-05-31 15:50:25 +0500

    визуал

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,14 +1,16 @@
-import '../Main/Main.css'
-import '../Halfs/Halfs'
-import Halfs from '../Halfs/Halfs'
-import '../Footer/Footer'
-import Footer from '../Footer/Footer'
-import { getHalfs } from '../../data/Halfs'
+import "../Main/Main.css";
+import "../Halfs/Halfs";
+import Halfs from "../Halfs/Halfs";
+import "../Footer/Footer";
+import Footer from "../Footer/Footer";
+import { getHalfs } from "../../data/Halfs";
+import { useState } from "react";
+import { useEffect } from "react";
 
-import photo10724 from '../../assets/photos/10724.jpg'
-import sitelogo from '../../assets/icons/site-icon.svg'
+import photo10724 from "../../assets/photos/10724.jpg";
+import sitelogo from "../../assets/icons/site-icon.svg";
 
-function Main(props){
+function Main(props) {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     useEffect(() => {
         const handleResize = () => {
@@ -21,26 +23,26 @@ function Main(props){
     }, []);
     return (
         <>
-            <div className='main'>
-                <div className='main_header'>
+            <div className="main">
+                <div className="main_header">
                     {/* <div className='header_left'>
                         Lorem, ipsum dolor.
                     </div> */}
-                    <div className='main_header_middle'>
-                        <p className='martian-mono'>Вкус&Традиция</p>
+                    <div className="main_header_middle">
+                        <p className="martian-mono">Вкус&Традиция</p>
                     </div>
                     {/* <div className='main_header_right'>
                         Lorem, ipsum dolor.
                     </div> */}
                 </div>
-                <div className='text'>
-                    <p className='oswald'>
-                        Выберите интересующий ресторан
-                    </p>
+                <div className="text">
+                    <p className="oswald">Выберите интересующий ресторан</p>
                 </div>
-                <div className='text_small'>
-                    <p className='comfortaa'>
-                        Быстрый ланч, уютный вечер вдвоем, шумные посиделки с компанией. В нашем меню есть всё, чтобы сделать ваш момент особенным.
+                <div className="text_small">
+                    <p className="comfortaa">
+                        Быстрый ланч, уютный вечер вдвоем, шумные посиделки с
+                        компанией. В нашем меню есть всё, чтобы сделать ваш
+                        момент особенным.
                     </p>
                 </div>
             </div>
@@ -93,28 +95,27 @@ function Main(props){
                 </>
             )}
 
-
-            <div className='main-about_half'>
-                <div className='first_half'>
-                    <div className='pic'>
+            <div className="main-about_half">
+                <div className="first_half">
+                    <div className="pic">
                         <img src={sitelogo} alt="" />
                     </div>
-                    <div className='first_half_desc_text'>
-                        <h1 className='martian-mono'>
-                            Вкус&Традиция
-                        </h1>
-                    </div> 
-                    <button className='first_half_desc_button' onClick={() => props.OnClick("About")}>
-                        <p className='montserrat'>О нас</p>
+                    <div className="first_half_desc_text">
+                        <h1 className="martian-mono">Вкус&Традиция</h1>
+                    </div>
+                    <button
+                        className="first_half_desc_button"
+                        onClick={() => props.OnClick("About")}
+                    >
+                        <p className="montserrat">О нас</p>
                     </button>
                 </div>
-                <img className='second_half' src={photo10724} alt="" />
+                <img className="second_half" src={photo10724} alt="" />
             </div>
-
 
             <Footer></Footer>
         </>
-      )
+    );
 }
 
-export default Main
+export default Main;
